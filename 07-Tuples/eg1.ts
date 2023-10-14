3ff3f35cde6ee7604f7a69a94151291653fe20c0
Abhilash Bhosle <abhilashbhosle10@gmail.com>
2023-10-14 17:48:55 +0530

    initial code

---
+++ 07-Tuples/eg1.ts
@@ -0,0 +1,13 @@
+// Tuples are basically meant for following the order 
+
+let user:[string,boolean,number]
+
+user=["abhi",true,1]
+
+// user=[1,"hey",false]  this throws an error as it is not following the order declared
+
+type db=[number,string]
+let consumer:db=[1,'ab@ab.com']
+
+let rgb:[number,number,number]
+rgb=[255,255,255]
